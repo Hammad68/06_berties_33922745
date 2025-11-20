e45a45a14dbcd2952751184c9b33730e1799ffc8
Hammad Ahmed <hammadahmed682003@gmail.com>
2025-11-20 22:29:32 +0000

    Database Credential Variables Update

--- index.js
+++ index.js
@@ -23,12 +23,12 @@ app.locals.shopData = {shopName: "Bertie's Books"}
 
 // Define the database connection pool
 const db = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
+    host: 'localhost',
+    user: process.env.BB_USER,
+    password: process.env.BB_PASSWORD,
+    database: process.env.BB_DATABASE,
     waitForConnections: true,
-    connectionLimit: parseInt(process.env.DB_CONNECTION_LIMIT, 10) || 10,
+    connectionLimit: 10,
     queueLimit: 0,
 });
 global.db = db;
