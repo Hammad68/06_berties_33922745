d64ae568bc684760907c36f1284291f811b285c7
Hammad Ahmed <hammadahmed682003@gmail.com>
2025-11-28 05:00:40 +0000

    Added express-session and express sanitizer logic & secured database host credential.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Import express and ejs
 require('dotenv').config()
+var session = require('express-session')
 var express = require ('express')
 var ejs = require('ejs')
 const path = require('path')
@@ -8,9 +9,13 @@ var mysql = require('mysql2')
 // Create the express application object
 const app = express()
 const port = 8000
+const expressSanitizer = require('express-sanitizer');
 
 // Tell Express that we want to use EJS as the templating engine
 app.set('view engine', 'ejs')
+
+// Create an input sanitizer
+app.use(expressSanitizer());
 
 // Set up the body parser 
 app.use(express.urlencoded({ extended: true }))
@@ -23,7 +28,7 @@ app.locals.shopData = {shopName: "Bertie's Books"}
 
 // Define the database connection pool
 const db = mysql.createPool({
-    host: 'localhost',
+    host: process.env.BB_HOST,
     user: process.env.BB_USER,
     password: process.env.BB_PASSWORD,
     database: process.env.BB_DATABASE,
@@ -33,6 +38,15 @@ const db = mysql.createPool({
 });
 global.db = db;
 
+// Create a session
+app.use(session({
+    secret: 'somerandomstuff',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        expires: 600000
+    }
+}))
 
 // Load the route handlers
 const mainRoutes = require("./routes/main")
