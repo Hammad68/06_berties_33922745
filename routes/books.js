a04015ac3230f4b1100d57a00c0231ba629a9be1
Hammad Ahmed <hammadahmed682003@gmail.com>
2025-11-28 09:00:51 +0000

    books redirect route updated

--- routes/books.js
+++ routes/books.js
@@ -5,11 +5,12 @@ const { check, validationResult, query } = require('express-validator');
 
 const redirectLogin = (req, res, next) => {
     if (!req.session.userId ) {
-      return res.redirect('/users/login') // redirect to the login page
+      return res.redirect('../users/login') // redirect to the login page
     } else { 
         next (); // move to the next middleware function
     } 
 }
+
 
 router.get('/search',function(req, res, next){
     res.render("search.ejs")
